081abebf7242f981a24f57bab6e6094b54c21229
Stanley Huang <huang.stanley050@gmail.com>
2019-10-19 21:30:43 +1100

    able to connect Result component to redux

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const ResultPage = props => {
+  console.log(props.wizard);
   return <h1>Result page</h1>;
 };
-
-export default ResultPage;
+const mapStateToProps = state => ({
+  wizard: state.form.wizard
+});
+export default connect(mapStateToProps)(ResultPage);
