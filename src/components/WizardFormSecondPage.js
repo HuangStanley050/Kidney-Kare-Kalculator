a6d4c84863a35320f82e6466460558310cf2ca10
Stanley Huang <huang.stanley050@gmail.com>
2019-10-20 09:13:48 +1100

    second page done

--- src/components/WizardFormSecondPage.js
+++ src/components/WizardFormSecondPage.js
@@ -1,41 +1,134 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
-import { Form, Button } from "reactstrap";
+import { Form, Button, FormGroup, Label } from "reactstrap";
 import validate from "./validate";
 import renderField from "./renderField";
 
 const renderError = ({ meta: { touched, error } }) =>
   touched && error ? <span>{error}</span> : false;
 
+/*
+<Form onSubmit={handleSubmit}>
+  <Field
+    name="Alcohol"
+    type="text"
+    component={renderField}
+    label="Do you drink alchol?"
+  />
+  <Field
+    name="Energy"
+    type="text"
+    component={renderField}
+    label="Do you feel more tired and have less energy?"
+  />
+  <Field
+    name="Concentration"
+    type="text"
+    component={renderField}
+    label="Do you have problem concentrating?"
+  />
+  <Field
+    name="Appetite"
+    type="text"
+    component={renderField}
+    label="Do you have poor appetite?"
+  />
+ */
+
 const WizardFormSecondPage = props => {
   const { handleSubmit, previousPage } = props;
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Field
-        name="Alcohol"
-        type="text"
-        component={renderField}
-        label="Do you drink alchol?"
-      />
-      <Field
-        name="Energy"
-        type="text"
-        component={renderField}
-        label="Do you feel more tired and have less energy?"
-      />
-      <Field
-        name="Concentration"
-        type="text"
-        component={renderField}
-        label="Do you have problem concentrating?"
-      />
-      <Field
-        name="Appetite"
-        type="text"
-        component={renderField}
-        label="Do you have poor appetite?"
-      />
 
+  return (
+    <Form>
+      <FormGroup>
+        <FormGroup>
+          <Label>How much water do you drink a day?</Label>
+        </FormGroup>
+        <FormGroup>
+          <Field name="Water" component="input" type="radio" value="6-8" /> 6-8
+        </FormGroup>
+        <FormGroup>
+          <Label check>
+            <Field name="Water" component="input" type="radio" value="4-6" />{" "}
+            4-6
+          </Label>
+        </FormGroup>
+        <FormGroup>
+          <Label>
+            <Field name="Water" component="input" type="radio" value="1-3" />{" "}
+            1-3
+          </Label>
+        </FormGroup>
+      </FormGroup>
+
+      <FormGroup>
+        <FormGroup>
+          <Label>Do you drink alcohol?</Label>
+        </FormGroup>
+        <FormGroup>
+          <Label>
+            <Field name="Alcohol" component="input" type="radio" value="Yes" />
+          </Label>{" "}
+          Yes
+        </FormGroup>
+        <FormGroup>
+          <Label>
+            <Field name="Alcohol" component="input" type="radio" value="No" />
+          </Label>{" "}
+          No
+        </FormGroup>
+      </FormGroup>
+
+      <FormGroup>
+        <FormGroup>
+          <Label>Do you feel tired and have less energy?</Label>
+        </FormGroup>
+        <FormGroup>
+          <Label>
+            <Field
+              name="Energy"
+              component="input"
+              type="radio"
+              value="Yes"
+            ></Field>
+          </Label>{" "}
+          Yes
+        </FormGroup>
+        <FormGroup>
+          <Label>
+            <Field
+              name="Energy"
+              component="input"
+              type="radio"
+              value="No"
+            ></Field>
+          </Label>{" "}
+          No
+        </FormGroup>
+      </FormGroup>
+      <FormGroup>
+        <FormGroup>
+          <Label>Do you have trouble concentrating?</Label>
+        </FormGroup>
+        <FormGroup>
+          <Field
+            name="Concentration"
+            component="input"
+            type="radio"
+            value="Yes"
+          ></Field>{" "}
+          Yes
+        </FormGroup>
+        <FormGroup>
+          <Field
+            name="Concentration"
+            component="input"
+            type="radio"
+            value="No"
+          ></Field>{" "}
+          No
+        </FormGroup>
+      </FormGroup>
       <div>
         <Button type="button" className="previous" onClick={previousPage}>
           Previous
