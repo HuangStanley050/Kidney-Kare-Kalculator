ab83862903bbbcf4a8467ff16192e38d684e3814
Stanley Huang <huang.stanley050@gmail.com>
2019-10-20 11:34:02 +1100

    added logo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Container } from "reactstrap";
 import Result from "./components/Result";
 import WizardForm from "./components/WizardForm";
+import Logo from "./assets/Logo.jpg";
 //import showResults from "./components/showResults";
 
 function App() {
@@ -10,7 +11,12 @@ function App() {
   return (
     <div>
       <h1 style={{ textAlign: "center", marginTop: "2rem" }}>
-        Kidney KareCalc
+        Kidney KareCalc{" "}
+        <img
+          style={{ width: "90px", borderRadius: "50%" }}
+          src={Logo}
+          alt="logo"
+        />
       </h1>
       <Container>
         {result ? <Result /> : <WizardForm onSubmit={() => showResult(true)} />}
